634b17a1533a091c13f0b5f32f5cecc47f16440f
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-09-19 16:10:23 -0300

    feat: create connect calendar page

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { z } from 'zod'
 
 import { Container, Form, FormError, Header } from './styles'
 import { api } from '@/src/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -48,8 +49,15 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
